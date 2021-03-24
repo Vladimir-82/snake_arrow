41dbfb70a0d97834aacc67b3c692adc12f8b3b75
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-03-24 16:53:51 +0300

    Initial commit

---
+++ snake.py
@@ -0,0 +1,21 @@
+# n = 3
+# arrow = []
+# roud = []
+#
+#
+# for i in range(1, n * n + 1):
+#
+#     roud.append(i)
+#     if len(roud) >= n:
+#
+#         arrow.append(roud)
+#         roud.clear()
+# print(arrow)
+
+n = 3
+arrow = n * []
+counter = 0
+roud = []
+for i in range(1, n * n + 1):
+    if len(roud) >= n:
+        arrow[]
