bf2a8cd572b15b3a98c531a468c587d57fdaf996
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-03-25 16:52:20 +0300

    begin

--- snake_in_side.py
+++ snake_in_side.py
@@ -1,12 +1,27 @@
+def up():
+    pass
+def right():
+    pass
+def down():
+    pass
+def left():
+    pass
+
+
 n = 3
-arrow = [[0]*3 for i in range(n)]
-counter_first = 0
-counter_second = 0
+arrow = [[0]*n for i in range(n)]
+index_first = 0
+index_second = 0
+round = 0
 for i in range(1, n * n + 1):
-    arrow[counter_first][counter_second] = i
-    counter_second += 1
-    if counter_second == n:
-        break
+    up()
+    # arrow[index_first][index_second] = i
+    # index_second += 1
+    #
+    # if index_second == n - 1:
+    #     index_second = n - 1 - round
+    #     index_first += 1
+
 
 
 print(arrow)
