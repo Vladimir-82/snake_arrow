d2cc547f51189bbfed71b49205827651655c0e93
vladimir <grun_gespenst@tut.by>
2021-03-28 15:50:56 +0300

    snake_ready

--- snake_in_side.py
+++ snake_in_side.py
@@ -1,72 +1,62 @@
-n = 3
-arrow = [[0]*n for i in range(n)]
+def to_right(arrow, index_first, index_second, elem):
+    while True:
+        arrow[index_first][index_second] = elem
+        index_second += 1
+        elem += 1
+        if index_second == n or arrow[index_first][index_second] != 0:
+            return arrow, index_first + 1, index_second - 1, elem
 
+def to_down(arrow, index_first, index_second, elem):
+    while True:
+        arrow[index_first][index_second] = elem
+        index_first += 1
+        elem += 1
+        if index_first == n or arrow[index_first][index_second] != 0:
+            return arrow, index_first - 1, index_second - 1, elem
+
+def to_left(arrow, index_first, index_second, elem):
+    while True:
+        arrow[index_first][index_second] = elem
+        index_second -= 1
+        elem += 1
+
+        if arrow[index_first][index_second] != 0:
+            return arrow, index_first - 1, index_second + 1, elem
+        if index_second == 0:
+            return arrow, index_first, index_second, elem
+
+def to_up(arrow, index_first, index_second, elem):
+    while True:
+        arrow[index_first][index_second] = elem
+        index_first -= 1
+        elem += 1
+        if arrow[index_first][index_second] != 0:
+            return arrow, index_first + 1, index_second + 1, elem
+
+n = 4
+arrow = [[0]*n for _ in range(n)]
 index_first = 0
 index_second = 0
-
-round = 0
-start = 1
-finish = n
-counter = 0
-for k in range(len(arrow)):
-    while 0 in arrow[k]:
-
-        if round != 0:
-            start = finish
-            finish = start + (n - 1 - round)
-
-        for i in range(start, finish):
-            arrow[index_first][index_second] = i
-            counter += 1
-            index_second += 1
-            if index_second == n - round:
-                break
-        if counter >= n * n:
-            break
-
-
-        start = finish
-        finish = start + (n - 1 - round)
-
-        for i in range(start, finish):
-
-            arrow[index_first][index_second] = i
-            counter += 1
-
-            index_first += 1
-            if index_first == n - round:
-                break
-        if counter >= n * n:
-            break
-
-
-        start = finish
-        finish = start + (n - 1 - round)
-
-        for i in range(start, finish):
-
-            arrow[index_first][index_second] = i
-            counter += 1
-            index_second -= 1
-            if index_second == round:
-                break
-        if counter >= n * n:
-            break
-
-        start = finish
-        finish = start + (n - 2 - round)
-        for i in range(start, finish):
-            arrow[index_first][index_second] = i
-            counter += 1
-            index_first -= 1
-            if index_first == round:
-                break
-        if counter >= n * n:
-            break
-
-    round += 1
+elem = 1
+while True:
+    arrow, index_first, index_second, elem = to_right(arrow, index_first, index_second, elem)
+    if elem >= n * n + 1:
+        break
+    arrow, index_first, index_second, elem = to_down(arrow, index_first, index_second, elem)
+    if elem >= n * n + 1:
+        break
+    arrow, index_first, index_second, elem = to_left(arrow, index_first, index_second, elem)
+    if elem >= n * n + 1:
+        break
+    arrow, index_first, index_second, elem = to_up(arrow, index_first, index_second, elem)
+    if elem >= n * n + 1:
+        break
 
 print(arrow)
+
+
+
+
 
 
 
